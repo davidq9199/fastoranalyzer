7cee1aea068188d9131330859c82b0a5f46734db
david9199 <davidq9199@gmail.com>
2024-08-07 22:11:08 -0400

    DOC: Add concise docstrings

--- fastoranalysis/factor_analysis.py
+++ fastoranalysis/factor_analysis.py
@@ -2,12 +2,43 @@ import numpy as np
 from scipy import linalg, optimize
 
 class FactorAnalysis:
+    """
+    Factor Analysis using Maximum Likelihood Estimation.
+
+    Parameters
+    ----------
+    n_factors : int
+        Number of factors to extract.
+
+    Attributes
+    ----------
+    loadings_ : ndarray of shape (n_features, n_factors)
+        Factor loadings matrix.
+    uniquenesses_ : ndarray of shape (n_features,)
+        Uniquenesses of each feature.
+
+    """
+
     def __init__(self, n_factors):
         self.n_factors = n_factors
         self.loadings_ = None
         self.uniquenesses_ = None
 
     def fit(self, X):
+        """
+        Fit the factor analysis model.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data.
+
+        Returns
+        -------
+        self : object
+            Fitted estimator.
+        
+        """
         n_samples, n_features = X.shape
         corr = np.corrcoef(X, rowvar=False)
         
@@ -28,6 +59,20 @@ class FactorAnalysis:
         return self
 
     def transform(self, X):
+        """
+        Apply dimensionality reduction to X using the fitted model.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Data to transform.
+
+        Returns
+        -------
+        X_new : ndarray of shape (n_samples, n_factors)
+            Transformed data.
+            
+        """
         if self.loadings_ is None:
             raise ValueError("FactorAnalysis must be fitted before transform")
         return X @ self.loadings_
